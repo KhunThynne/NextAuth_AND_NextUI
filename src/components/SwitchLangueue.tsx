40cf551b67474045ff1c1a3bbb014edf5f5c031f
Thynne <khunthynne@gmail.com>
2024-09-03 23:45:21 +0700

    add

--- src/components/SwitchLangueue.tsx
+++ src/components/SwitchLangueue.tsx
@@ -1,24 +1,20 @@
 "use client";
 import { Button } from "@nextui-org/button";
 import { useParams, useRouter } from "next/navigation";
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 export default function SwitchLangueue() {
     const router = useRouter();
     const { locale } = useParams();
 
     const switchLocale = useCallback(() => {
-        // Check current locale and switch to the other one
         const newLocale = locale === "en" ? "th" : "en";
 
-        // Get current pathname
         const pathname = window.location.pathname;
-
-
         const newPathname = pathname.replace(`/${locale}`, `/${newLocale}`);
-
+ 
         router.push(newPathname);
         router.refresh();
-    }, []);
+    }, [locale]);
 
     return (
         <div className="flex gap-2">
